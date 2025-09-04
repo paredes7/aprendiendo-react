6917f25ca5ec5f256720ce3e154143199027274d
Paredes <202301667@est.umss.edu>
2025-09-03 21:02:10 -0400

    Actualización: proyecto hola-mundo mejorado

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -1,26 +1,68 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css' //importamos aqui a nivel de "COMPONENTE"
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+
+//renderizado de listas
+    const users = [
+    {
+        userName: 'jhoel',
+        name: 'jhoel paredes pava',
+        isFollowing: true
+    },
+    {
+        userName: 'anabel',
+        name: 'anabel paredes pava',
+        isFollowing: false
+    },
+    {
+        userName: 'jhasef',
+        name: 'jhaseft rene',
+        isFollowing: true
+    },
+    {
+        userName: 'ale',
+        name: 'ale zerna',
+        isFollowing: false
+    }
+    ]
+    
+
 //export exporta nuesto componente y en main lo importamos
 export function App(){
-    /*clasname para que nuestro componente sea reutilizable
-    swit = es un tipo de prefijo
-    swit = 'tw-followCard'  ---> es el nombre que esta en App.css
+    //formato del userName que llama a FormatUserName de TwitterFollowCard.jsx
+    //const format = (userName) => `@${userName}`;
 
-    className='tw-followCard-header' ---> lo podemos estilar en header
-    className='tw-followCard-avatar' ---> para estilar imagen
-
-    */
 
     //<react.fragment><react.fragment/>  ==  <></>
+
+    /*const [isFollowing, setIsFollowing] = useState(false)
+
+    console.log("[app] render con isFollowing: ", isFollowing)*/
+
+
     return(
-        <>
-            <TwitterFollowCard userName="jhoel" name="jhoel paredes pava"/>
-            <TwitterFollowCard userName="jhoel" name="jhoel paredes pava"/>
-        </>
+        //clasname=App   es la separacion que lo tendremos a nivel de "'CONTENEDOR'"
+        //'isFollowing' pasamos de la siguiente manera --->  isFollowing={true} ==isFollowing
+       //formatUserName={format} --> pasamos una funcion
+       <section className='App'>
+            {
+                users.map(user=> {
+                    const{userName, name, isFollowing} = user
+                    return(
+                        <TwitterFollowCard
+                            key={userName}
+                            userName = {userName}
+                            initialIsFollowing={isFollowing}
+                            >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
     )
         
 }
--- projects/hola-mundo/src/TwitterFollowCard.jsx
+++ projects/hola-mundo/src/TwitterFollowCard.jsx
@@ -1,11 +1,35 @@
-//esto es un "COMPONENTE" reuzable = no
+//esto es un "COMPONENTE" reuzable = si
 /* la base de la reutilizacion de los componentes en react son los "PARAMETROS/PROPIEDADES"
 ejemplo: export function TwitterFollowCard({userName, name, ...PARAMETROS}){
 **/
 
+/*clasname para que nuestro componente sea reutilizable
+    swit = es un tipo de prefijo
+    swit = 'tw-followCard'  ---> es el nombre que esta en App.css
+    className='tw-followCard-header' ---> lo podemos estilar en header
+    className='tw-followCard-avatar' ---> para estilar imagen
+    */
 
+//src={'https://unavatar.io/${userName}'}/> ---> la exprecion se evalua con llaves
+//isFollowing es booleano como psamos a App.jsx
 
-export function TwitterFollowCard({userName, name, isFollowing}){
+//importamos {useState para dar funcionalidad al componente (Host)}
+import { useState } from "react"
+
+export function TwitterFollowCard({children, userName, initialIsFollowing}){
+    //para useState
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)//valor de estado inicial
+
+    //Renderizado condicional
+    const text = isFollowing ? 'siguiendo' : 'Seguir'
+    //para estilos de estar siguiendo o no
+    const buttonClasName = isFollowing ? 'tw-followCard-button is-following' 
+    : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)//cambia de false a true o viseversa
+    }
+
     return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
@@ -15,7 +39,7 @@ export function TwitterFollowCard({userName, name, isFollowing}){
                 src={'https://unavatar.io/${userName}'}/>
                 <div className='tw-followCard-info'>
                     <strong>
-                        {name}
+                        {children}
                     </strong>
                     <span className="tw-followCard-infoUserName">
                         @{userName}
@@ -24,8 +48,11 @@ export function TwitterFollowCard({userName, name, isFollowing}){
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>
-                    seguir
+                <button className={buttonClasName} onClick={handleClick}>
+                    {text}
+                    <span className="tw-followCar-stopFollow">
+                        Dejar de seguir
+                    </span>
                 </button>
             </aside>
         </article>
